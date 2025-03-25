4ba52b0ac5875f2f8a65b1923da231c97dbf253c
qwerty <93528492+CodeAkyra@users.noreply.github.com>
2025-03-25 23:12:54 +0800

    minor changes

--- product_transaction.php
+++ product_transaction.php
@@ -1,33 +1,39 @@
 <title>Product Transaction</title>
-<!-- Tentative Title Page -->
 
 <?php
-
 require "includes/conn.php";
 
-$product_id = $_GET['id'];
+if (isset($_GET['id']) && is_numeric($_GET['id'])) {
+    $product_id = $_GET['id'];
 
-$poIQuery = "SELECT po_i.product_id, po_i.id, po_i.po_id, po_i.quantity, po_i.price, po_i.subtotal, po.date_created, p.name, c.name as customer_name
-                FROM purchase_order_items po_i
-                LEFT JOIN purchase_orders po ON po_i.po_id = po.id
-                LEFT JOIN customers c ON po.customer_id = c.id
-                LEFT JOIN products p ON po_i.product_id = p.id
-                WHERE po_i.product_id = $product_id";
+    $poIQuery = "SELECT po_i.product_id, po_i.id, po_i.po_id, po_i.quantity, po_i.price, po_i.subtotal, po.date_created, p.name, c.name as customer_name
+    FROM purchase_order_items po_i
+    LEFT JOIN purchase_orders po ON po_i.po_id = po.id
+    LEFT JOIN customers c ON po.customer_id = c.id
+    LEFT JOIN products p ON po_i.product_id = p.id
+    WHERE po_i.product_id = $product_id";
 
-$poIResult = mysqli_query($conn, $poIQuery);
+    $poIResult = mysqli_query($conn, $poIQuery);
 
+    if (mysqli_num_rows($poIResult) > 0) {
+        $fetchName = mysqli_fetch_array($poIResult);
+        $productName = htmlspecialchars($fetchName['name']);
+    } else {
+        $productName = "No Transactions Yet";
+    }
+} else {
+    echo "<div class='alert alert-danger'>No transactions found</div>";
+    exit;
+}
 ?>
 
 <div>
-    <h1>Transaction for - <?php $fetchName = mysqli_fetch_array($poIResult);
-                            echo $fetchName['name'] ?></h1>
+    <h1>Purchase Records for - <?php echo $productName; ?></h1> <!-- tentative, pero baka eto gamitin yung name para sa module na toh or page na toh -->
     <a href="inventory.php" class="btn btn-primary">Return</a>
 </div>
 
-
 <table class="table">
     <tr>
-        <!-- <th>COS ID</th> TENTATIVE -->
         <th>Product Name</th>
         <th>Customer Name</th>
         <th>COS Date Created</th>
@@ -38,23 +44,23 @@ $poIResult = mysqli_query($conn, $poIQuery);
     </tr>
 
     <?php
-
-    if (mysqli_num_rows($poIResult) > 0) {
+    if (isset($poIResult) && mysqli_num_rows($poIResult) > 0) {
+        mysqli_data_seek($poIResult, 0);
         while ($row = mysqli_fetch_array($poIResult)) {
             echo "<tr>
-        <!-- <td> " . htmlspecialchars($row["po_id"]) . " </td>  tentative -->
-        <td> " . htmlspecialchars($row["name"]) . " </td>
-        <td> " . htmlspecialchars($row["customer_name"]) . " </td>
-        <td> " . htmlspecialchars($row["date_created"]) . " </td>
-        <td> " . htmlspecialchars($row["quantity"]) . " </td>
-        <td> " . htmlspecialchars($row["price"]) . " </td>
-        <td> " . htmlspecialchars($row["subtotal"]) . " </td>
-        <td>
-        <a href='view_po.php?id=" . $row['po_id'] . "' class='btn btn-outline-dark' >View</a>
-        </td>
-        </tr>";
+                <td>" . htmlspecialchars($row["name"]) . "</td>
+                <td>" . htmlspecialchars($row["customer_name"]) . "</td>
+                <td>" . htmlspecialchars($row["date_created"]) . "</td>
+                <td>" . htmlspecialchars($row["quantity"]) . "</td>
+                <td>" . htmlspecialchars($row["price"]) . "</td>
+                <td>" . htmlspecialchars($row["subtotal"]) . "</td>
+                <td>
+                    <a href='view_po.php?id=" . $row['po_id'] . "' class='btn btn-outline-dark'>View</a>
+                </td>
+            </tr>";
         }
+    } else {
+        echo "<tr><td colspan='7' class='text-center'>No transactions found</td></tr>";
     }
-
     ?>
 </table>
